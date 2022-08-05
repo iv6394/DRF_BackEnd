6ef53ffb11d184307a6e00b99b9f16640a10ea22
iv6394 <i.vorobev@yahoo.com>
2022-08-05 18:37:06 +0300

    created api queryset and json-style output

--- books/store/views.py
+++ books/store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from store.serializers import BooksSerializer
+from rest_framework.viewsets import ModelViewSet
+from store.models import Book
 
 # Create your views here.
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
